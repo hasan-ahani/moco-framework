c3c6f361634a502c60813515da075fd28a92fdc5
hasan ahani <hasanahani.ir@gmail.com>
2021-03-31 18:00:02 +0430

    Update options.php

--- Core/View/options.php
+++ Core/View/options.php
@@ -29,9 +29,9 @@ $options = $option->getOptions();
                 </small>
             </div>
             <ul class="header-actions">
-                <li>
-                    <input id="search-controls" type="text" class="mc-control" placeholder="<?php _e('search ...');?>">
-                </li>
+<!--                <li>-->
+<!--                    <input id="search-controls" type="text" class="mc-control" placeholder="--><?php //_e('search ...');?><!--">-->
+<!--                </li>-->
                 <li>
                     <input type="hidden" name="nonce" value="<?php echo wp_create_nonce($option->getAction());?>">
                     <input type="hidden" name="action" value="<?php echo $option->getAction();?>">
@@ -82,111 +82,6 @@ $options = $option->getOptions();
                         </div>
                         
                     <?php endforeach; ?>
-    <!--                <div class="moco-tab-content show">-->
-    <!--                    <div class="form-inline">-->
-    <!--                        <div class="form-label">-->
-    <!--                            <label for="radio">radio button</label>-->
-    <!--                            <small>input test</small>-->
-    <!--                        </div>-->
-    <!--                        <div class="form-control-box">-->
-    <!--                            <div class="form-control" id="radio">-->
-    <!--                                <label class="mc-radio">-->
-    <!--                                    <input type="radio" name="test" value="test" class="mc-control">-->
-    <!--                                    <span>test</span>-->
-    <!--                                </label>-->
-    <!--                                <label class="mc-radio">-->
-    <!--                                    <input type="radio" name="test" value="admin" class="mc-control">-->
-    <!--                                    <span>test</span>-->
-    <!--                                </label>-->
-    <!--                            </div>-->
-    <!--                            <small>input textarea</small>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                    <div class="form-inline">-->
-    <!--                        <div class="form-label">-->
-    <!--                            <label for="switch">switch</label>-->
-    <!--                        </div>-->
-    <!--                        <div class="form-control-box">-->
-    <!--                            <div class="form-control">-->
-    <!--                                <div class="mc-switch">-->
-    <!--                                    <input id="switch" type="checkbox" class="mc-control">-->
-    <!--                                    <label for="switch">Toggle this switch element</label>-->
-    <!--                                </div>-->
-    <!--                            </div>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                    <div class="form-inline">-->
-    <!--                        <div class="form-label">-->
-    <!--                            <label for="checkbox">checkbox</label>-->
-    <!--                            <small>input test</small>-->
-    <!--                        </div>-->
-    <!--                        <div class="form-control-box">-->
-    <!--                            <div class="form-control inline" id="checkbox">-->
-    <!--                                <label class="mc-checkbox">-->
-    <!--                                    <input type="checkbox" name="test" value="test" class="mc-control">-->
-    <!--                                    <span>test</span>-->
-    <!--                                </label>-->
-    <!--                                <label class="mc-checkbox">-->
-    <!--                                    <input type="checkbox" name="test" value="admin" class="mc-control">-->
-    <!--                                    <span>test</span>-->
-    <!--                                </label>-->
-    <!--                            </div>-->
-    <!--                            <small>input textarea</small>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                    <div class="form-inline">-->
-    <!--                        <div class="form-label">-->
-    <!--                            <label for="checkbox">button</label>-->
-    <!--                            <small>input test</small>-->
-    <!--                        </div>-->
-    <!--                        <div class="form-control-box">-->
-    <!--                            <div class="form-control" id="button">-->
-    <!--                                <button class="mc-btn">Text Button</button>-->
-    <!--                            </div>-->
-    <!--                            <small>input textarea</small>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                    <div class="form-inline">-->
-    <!--                        <div class="form-label">-->
-    <!--                            <label for="checkbox">image select</label>-->
-    <!--                            <small>input test</small>-->
-    <!--                        </div>-->
-    <!--                        <div class="form-control-box">-->
-    <!--                            <div class="form-control row" id="button">-->
-    <!--                                <input type="url" class="mc-control">-->
-    <!--                                <button class="mc-btn btn-danger btn-clear btn-icon" data-moco-clear="button">-->
-    <!--                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">-->
-    <!--                                        <path d="M3.35288 8.95043C4.00437 6.17301 6.17301 4.00437 8.95043 3.35288C10.9563 2.88237 13.0437 2.88237 15.0496 3.35288C17.827 4.00437 19.9956 6.17301 20.6471 8.95044C21.1176 10.9563 21.1176 13.0437 20.6471 15.0496C19.9956 17.827 17.827 19.9956 15.0496 20.6471C13.0437 21.1176 10.9563 21.1176 8.95044 20.6471C6.17301 19.9956 4.00437 17.827 3.35288 15.0496C2.88237 13.0437 2.88237 10.9563 3.35288 8.95043Z" stroke="currentColor" stroke-width="1.5"/>-->
-    <!--                                        <path d="M13.7678 10.2322L10.2322 13.7677M13.7678 13.7677L10.2322 10.2322" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>-->
-    <!--                                    </svg>-->
-    <!--                                </button>-->
-    <!--                                <button class="mc-btn" data-moco-selection="button">Text Button</button>-->
-    <!--                            </div>-->
-    <!--                            <small>input textarea</small>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                    <div class="form-inline">-->
-    <!--                        <div class="form-label">-->
-    <!--                            <label for="checkbox">wp editor</label>-->
-    <!--                            <small>test</small>-->
-    <!--                        </div>-->
-    <!--                        <div class="form-control-box">-->
-    <!--                            <div class="form-control" id="button">-->
-    <!--                                --><?php
-    //                                    $content = "";
-    //                                    $editor_id = "e_id";
-    //                                    $editor_settings = array(
-    //                                        'teeny' => false,
-    //                                        'editor_height' => 160,
-    //                                        'quicktags' => array( 'buttons' => 'strong,em,del,close' ),
-    //                                        'media_buttons' => true );
-    //
-    //                                    wp_editor( $content, $editor_id ,$editor_settings );
-    //                                ?>
-    <!--                            </div>-->
-    <!--                        </div>-->
-    <!--                    </div>-->
-    <!--                </div>-->
                 </div>
                 
             </div>
